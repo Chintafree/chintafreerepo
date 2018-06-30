23c9dbd38c404c53e2cad47957089aa774bededf
bhaubhau <nuclear@123>
2018-06-30 20:17:08 +0530

    committing generated files

---
+++ java/chintafree-core-services/src/main/java/in/chintafree/chintafreecoreservices/ChintafreeCoreServicesApplication.java
@@ -0,0 +1,12 @@
+package in.chintafree.chintafreecoreservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChintafreeCoreServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChintafreeCoreServicesApplication.class, args);
+	}
+}
